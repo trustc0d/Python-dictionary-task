16c4a2e47c5b78bbe3f87ae2a4a10f848d54a878
trustc0d <evertrust.code@gmail.com>
2023-10-11 08:43:11 -0400

    [dev] added error handling and slight style changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
-openai.api_key = os.getenv('API_KEY')
+openai.api_key = os.getenv("API_KEY")
 openai.api_base = os.getenv("API_BASE")
 
 st.set_page_config(
@@ -15,14 +15,15 @@ st.set_page_config(
 )
 
 def get_definition(user_input):
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt = f"Can you explain {user_input} in simple terms, in one sentence? After the explanation, say something like '[It's/They're] essentially [explanation] 💡.' Please keep your response brief.",
-    temperature=0.5,
-    max_tokens=112,
-    )
-
-    return response.choices[0].text
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt = f"Can you explain {user_input} in simple terms, in one sentence? After the explanation, say something like '[It's/They're] essentially [explanation] 💡.' Please keep your response brief.",
+            temperature=0.5,
+            max_tokens=112,
+        )
+        return response.choices[0].text
+    except Exception as err: return None, err
 
 st.markdown("<h1><center>💡Simple Dictionary</center></h1>", unsafe_allow_html=True)
 st.markdown("<h6><center>Enter a word or phrase below to get a simple definition in seconds... 📖</center></h6>", unsafe_allow_html=True)
@@ -32,7 +33,15 @@ chat_button = st.columns((5, 3, 5))[1].button("Get Definition ✨")
 st.write("")
 if chat_button and user_input.strip() != "":
     with st.spinner("Loading... 🤓"):
-        st.info(get_definition(user_input))
+        definition, err = get_definition(user_input)
+        if definition: st.info(definition)
+        else: st.error(f"🚨 Error: {err}")
 
 st.markdown("<hr><center>© Omar Anwar 2023</center><hr>", unsafe_allow_html=True)
-st.markdown("<style> footer {visibility: hidden;} </style>", unsafe_allow_html=True)
+st.markdown("""
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """, unsafe_allow_html=True)
